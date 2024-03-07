db91968c5d4b2a3829255a8311d900e3a382286a
47012 <124239348+47012@users.noreply.github.com>
2024-03-07 09:52:13 +0200

    Pitäisi toimia

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import React, { useState, useRef } from 'react';
-import MenuIcon from './images/icon50.png';
-import './App.css';
-import './content.css'
-import Content from './content';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
 
-export default function ExampleCss() {
-  const [content, setContent] = useState('Initial Content');
-  const wrapperRef = useRef();
-
-  const handleClick = () => {
-    const wrapper = wrapperRef.current;
-    wrapper.classList.toggle('is-nav-open');
-  };
-
-  const updateContent = (newContent) => {
-    setContent(newContent);
-  };
-
+export default function App() {
   return (
-    <div className="main">
-      <div ref={wrapperRef} className="wrapper">
-        <div className="nav">
-          <img
-            src={MenuIcon}
-            alt="Menu Icon"
-            className="nav_icon"
-            onClick={handleClick}
-          />
-          <div className="nav_body">
-            <Content updateContent={updateContent} />
-          </div>
-        </div>
-      </div>
-
-      <div className="content"> 
-        <h1>Container 1</h1>
-        <div id="container1">{content}</div>
-        
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
