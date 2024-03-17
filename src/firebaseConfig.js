d9836fd690eac70975ae50fcb2d0f17aeca7de73
EmilLammela <163102816+EmilLammela@users.noreply.github.com>
2024-03-17 09:15:13 +0200

    .env added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,15 +8,14 @@ import "firebase/compat/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmtDJhZaq6KUc_MR5tsKbRvkEu_KANOQk",
-  authDomain: "reactcrud-9052d.firebaseapp.com",
-  databaseURL: "https://reactcrud-9052d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "reactcrud-9052d",
-  storageBucket: "reactcrud-9052d.appspot.com",
-  messagingSenderId: "668274956037",
-  appId: "1:668274956037:web:19f06bc4d0afd92bd1896c",
-  measurementId: "G-RY3L5GE6KY"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) {
