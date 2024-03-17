59c7b3156b7a55591594707f5155e5d229b31fb9
EmilLammela <163102816+EmilLammela@users.noreply.github.com>
2024-03-17 21:21:38 +0200

    1.4
    todo alustus

--- src/pages/to_do_list.js
+++ src/pages/to_do_list.js
@@ -18,22 +18,30 @@ const ToDoList = () => {
     };
 
     return (
-        <div className="content_container">
-            <div className="box" id="left" onDrop={drop} onDragOver={allowDrop}>
-                <div id="drag1" draggable="true" onDragStart={drag}>
-                <p>Draggable and Droppable</p>
+        
+        <div>
+            <div className="labels">
+                <div className="label">left</div>
+                <div className="label">Label 1</div>
+                <div className="label">Label 2</div>
+                <div className="label">Label 3</div>
+                <div className="label">right</div>
+            </div>
+        
+            <div className="content_container">
+                <div className="box" id="left" onDrop={drop} onDragOver={allowDrop}>
+                    <div id="drag1" draggable="true" onDragStart={drag}>
+                    <p>Draggable and Droppable</p>
+                    </div>
                 </div>
-            </div>
-
-            <div className="box" id="div" onDrop={drop} onDragOver={allowDrop}></div>
-            <div className="box" id="div" onDrop={drop} onDragOver={allowDrop}></div>
-            <div className="box" id="div" onDrop={drop} onDragOver={allowDrop}></div>
-            <div className="box" id="div" onDrop={drop} onDragOver={allowDrop}></div>
+        
+                <div className="box" id="div1" onDrop={drop} onDragOver={allowDrop}></div>
+                <div className="box" id="div2" onDrop={drop} onDragOver={allowDrop}></div>
+                <div className="box" id="div3" onDrop={drop} onDragOver={allowDrop}></div>
             
-            <div className="box" id="right" onDrop={drop} onDragOver={allowDrop}>
+                <div className="box" id="right" onDrop={drop} onDragOver={allowDrop}></div>
             </div>
-
-      </div>
+        </div>
     );
 };
 
